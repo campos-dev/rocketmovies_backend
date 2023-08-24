6ec25cf6c3cee5ea7b54b9ff6555052884695c11
Marcelo Campos <camposwebdev@gmail.com>
2023-08-24 14:44:43 -0300

    upload profile picture

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,10 +1,23 @@
 const usersRoutes = require("express").Router();
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
+const multer = require("multer");
+const uploadConfig = require("../config/upload");
+const upload = multer(uploadConfig.MULTER);
+
 const UsersControllers = require("../controllers/UsersControllers");
 const usersControllers = new UsersControllers();
 
+const UserAvatarController = require("../controllers/UserAvatarController");
+const userAvatarController = new UserAvatarController();
+
 usersRoutes.post("/", usersControllers.create);
 usersRoutes.put("/", ensureAuthenticated, usersControllers.update);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  userAvatarController.update
+);
 
 module.exports = usersRoutes;
--- src/server.js
+++ src/server.js
@@ -3,13 +3,18 @@ const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const express = require("express");
 const routes = require("./routes");
+const uploadConfig = require("./config/upload");
+const cors = require("cors");
 
 migrationsRun();
 
 const app = express();
 app.use(express.json());
 
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+
 app.use(routes);
+app.use(cors());
 
 app.use((error, req, res, next) => {
   if (error instanceof AppError) {
